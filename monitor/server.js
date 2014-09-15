70e95b306abd934af9df4aeed8c391b06c851d94
jos <wjosdejong@gmail.com>
2014-09-15 17:01:40 +0200

    Some minor tweaks

--- monitor/server.js
+++ monitor/server.js
@@ -11,7 +11,6 @@ console.log('conductor agent url:', CONDUCTOR_AGENT_URL);
 console.log('monitor agent url:  ', MONITOR_AGENT_URL);
 
 var monitorAgent = new MonitorAgent(MONITOR_AGENT_URL, {port: 3000});
-var ws = null;
 
 // create a sort of a proxy agent for the conductorAgent
 // TODO: replace this with a websocket transport as soon as this is implemented in evejs
@@ -19,9 +18,12 @@ var conductorProxyAgent = new eve.Agent(CONDUCTOR_AGENT_URL);
 conductorProxyAgent.extend('rpc', []);
 conductorProxyAgent.connect(eve.system.transports.getAll());
 
+// open a web socket to the conductor agent now
+connect();
+
 function connect() {
 // open a websocket pass incoming messages via the conductorAgent to the monitorAgent
-  ws = new WebSocket(CONDUCTOR_AGENT_URL + '?id=' + MONITOR_AGENT_URL);
+  var ws = new WebSocket(CONDUCTOR_AGENT_URL + '?id=' + MONITOR_AGENT_URL);
   ws.on('open', function () {
     console.log('Connected to the conductor agent');
   });
@@ -37,10 +39,11 @@ function connect() {
         });
   });
   ws.on('close', function (err) {
+    console.log('Error: Connection to the conductor agent closed');
     reconnect();
   });
   ws.on('error', function (err) {
-    console.log('Error: Failed to connect to the conductor agent or connection is broken');
+    console.log('Error: Failed to connect to the conductor agent');
     reconnect();
   });
 }
@@ -48,13 +51,10 @@ function connect() {
 function reconnect() {
   console.log('Reconnecting in ' + (RECONNECT_DELAY / 1000) + ' seconds...');
   setTimeout(connect, RECONNECT_DELAY);
-};
+}
 
-connect();
-
-
-// generate ran
 if (argv.random) {
+// emit a random note once a second
   var NOTES = {
     C4: 0,
     D4: 1,
@@ -67,9 +67,6 @@ if (argv.random) {
     D5: 8
   };
 
-  // emit a random note once a second
-  // TODO: replace this with connecting via the websocket to the conductor agent
-  //       to listen for real events
   var nr = 0;
   var arr = Object.keys(NOTES);
   setInterval(function () {
