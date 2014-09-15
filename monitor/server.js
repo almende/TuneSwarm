830637f93b0bfe8aa79ee822d87b61d9548b7ddb
jos <wjosdejong@gmail.com>
2014-09-15 16:56:35 +0200

    Implemented auto reconnect

--- monitor/server.js
+++ monitor/server.js
@@ -1,7 +1,9 @@
 var argv = require('yargs').argv;
+var WebSocket = require('ws');
 var eve = require('evejs');
 var MonitorAgent = require('./MonitorAgent');
 
+var RECONNECT_DELAY = 10000; // ms
 var CONDUCTOR_AGENT_URL = argv.conductor || 'ws://localhost:8082/ws/conductor';
 var MONITOR_AGENT_URL = 'monitor';
 
@@ -9,6 +11,7 @@ console.log('conductor agent url:', CONDUCTOR_AGENT_URL);
 console.log('monitor agent url:  ', MONITOR_AGENT_URL);
 
 var monitorAgent = new MonitorAgent(MONITOR_AGENT_URL, {port: 3000});
+var ws = null;
 
 // create a sort of a proxy agent for the conductorAgent
 // TODO: replace this with a websocket transport as soon as this is implemented in evejs
@@ -16,29 +19,41 @@ var conductorProxyAgent = new eve.Agent(CONDUCTOR_AGENT_URL);
 conductorProxyAgent.extend('rpc', []);
 conductorProxyAgent.connect(eve.system.transports.getAll());
 
+function connect() {
 // open a websocket pass incoming messages via the conductorAgent to the monitorAgent
-var WebSocket = require('ws');
-var ws = new WebSocket(CONDUCTOR_AGENT_URL + '?id=' + MONITOR_AGENT_URL);
-ws.on('open', function () {
-  console.log('Connected to the conductor agent');
-  //ws.send(JSON.stringify({method: 'registerAgent', params: {}}));
-});
-ws.on('message', function (data, flags) {
-  var rpc = JSON.parse(data);
-  console.log('received', data);
-  conductorProxyAgent.request('monitor', rpc)
-      .then(function (result) {
-        ws.send(JSON.stringify({id: rpc.id, result: result, error: null}));
-      })
-      .catch(function (err) {
-        ws.send(JSON.stringify({id: rpc.id, result: null, error: err.toString()}));
-      });
-});
-ws.on('error', function (err) {
-  console.log('Error: Failed to connect to the conductor agent');
-});
+  ws = new WebSocket(CONDUCTOR_AGENT_URL + '?id=' + MONITOR_AGENT_URL);
+  ws.on('open', function () {
+    console.log('Connected to the conductor agent');
+  });
+  ws.on('message', function (data, flags) {
+    var rpc = JSON.parse(data);
+    console.log('received', data);
+    conductorProxyAgent.request('monitor', rpc)
+        .then(function (result) {
+          ws.send(JSON.stringify({id: rpc.id, result: result, error: null}));
+        })
+        .catch(function (err) {
+          ws.send(JSON.stringify({id: rpc.id, result: null, error: err.toString()}));
+        });
+  });
+  ws.on('close', function (err) {
+    reconnect();
+  });
+  ws.on('error', function (err) {
+    console.log('Error: Failed to connect to the conductor agent or connection is broken');
+    reconnect();
+  });
+}
 
-//
+function reconnect() {
+  console.log('Reconnecting in ' + (RECONNECT_DELAY / 1000) + ' seconds...');
+  setTimeout(connect, RECONNECT_DELAY);
+};
+
+connect();
+
+
+// generate ran
 if (argv.random) {
   var NOTES = {
     C4: 0,
@@ -52,9 +67,9 @@ if (argv.random) {
     D5: 8
   };
 
-// emit a random note once a second
-// TODO: replace this with connecting via the websocket to the conductor agent
-//       to listen for real events
+  // emit a random note once a second
+  // TODO: replace this with connecting via the websocket to the conductor agent
+  //       to listen for real events
   var nr = 0;
   var arr = Object.keys(NOTES);
   setInterval(function () {
